2b17136355311991a2e6cca2d882ce8c746f171e
Cduartev <cayooduartevidal@gmail.com>
2026-01-17 10:19:45 -0300

    /// testando usando o java 25 e java 20 testando as funcionalidades /// Aqui voce vai ver os tipos de variaveis INT DUBLE CHAR BOOLEAN STRING ISSO DAQUI E O BASICO DO BASICO

---
+++ untitled/src/Program_java_25.java
@@ -0,0 +1,11 @@
+void main() {
+    IO.println("Ola Mundo!");
+}
+
+/// No java 25 isso ocorreu uma simplificacao no codigo ou seja nao presicamos mais do PSVM Para codificar em java aquele psvm gigante COMO:
+/// public class Program{
+///     public static void main(String[] args){
+///         System.out.println("Ola Mundo!");
+///     }
+///
+/// }
---
+++ untitled/src/variavies_java_aula_1.java
@@ -0,0 +1,19 @@
+public class variavies_java_aula_1 {
+    static void main() {
+        int idade = 25;
+        double altura = 1.68;
+        char sexo = 'M';
+        boolean ativo = true;
+        String mensagem = "Cuidado com esse usuario";
+
+
+        IO.println("A sua idade e " +idade);
+        System.out.println("A sua altura  " + altura);
+        System.out.println("O seu sexo e " + sexo);
+        IO.println("Usuario Ativo? " + ativo);
+        IO.println(mensagem);
+
+    }
+}
+/// testando usando o java 25 e java 20 testando as funcionalidades
+/// Aqui voce vai ver os tipos de variaveis INT DUBLE CHAR BOOLEAN STRING ISSO DAQUI E O BASICO DO BASICO
