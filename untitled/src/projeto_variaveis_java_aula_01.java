5c3b661ec59136ccccf26a3416aa3c46cc3b11c9
Cduartev <cayooduartevidal@gmail.com>
2026-01-17 11:02:45 -0300

    Exercicios de variaveis

---
+++ untitled/src/projeto_variaveis_java_aula_01.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+
+public class projeto_variaveis_java_aula_01 {
+    static void main() {
+        String produto1="Computador" ;
+        String produto2="Teclado";
+
+        int idade = 30;
+        int codigo = 2020;
+        char sexo = 'M';
+        double preco1 = 2100.50;
+        double preco2 = 650.50;
+        double medida = 54.3213;
+
+        System.out.println("Produtos: ");
+        System.out.println("o Preco do "+produto1 + " e igual a " + preco1);
+        System.out.println("O preco do "+produto2+" e igual a "+ preco2);
+
+        IO.println("Registro: " + idade + " Anos, codigo "+ codigo + " e Genero: " + sexo);
+
+        IO.print("Codigo com 8 casas deciamais: ");
+        System.out.printf("%.8f%n",medida);
+        IO.print("COM 3 CASAS DECIMAIS: ");
+        System.out.printf("%.3f%n",medida);
+        IO.print("COM PONTOS DECIMAIS: ");
+        Locale.setDefault(Locale.US);
+        IO.println(medida);
+    }
+}
+
+///  LOCALE SERVE PARA ADICIOAR OS PONTOS DECIMAIS DE ACORDO COM A LOCALIDADE
--- untitled/src/variavies_java_aula_1.java
+++ untitled/src/variavies_java_aula_1.java
@@ -1,20 +1,26 @@
+import java.util.Locale;
+
 public class variavies_java_aula_1 {
     static void main() {
         int idade = 25;
-        double altura = 1.68;
+        double altura = 1.744444;
         char sexo = 'M';
         boolean ativo = true;
-        String mensagem = "Cuidado com esse usuario";
+        String nome = "Cayo";
 
-
+        Locale.setDefault(Locale.US);
         IO.println("A sua idade e " +idade);
-        System.out.println("A sua altura  " + altura);
+        System.out.printf("Sua altura %.2f%n", altura);
         System.out.println("O seu sexo e " + sexo);
         IO.println("Usuario Ativo? " + ativo);
-        IO.println(mensagem);
+        IO.println(nome);
+
+        System.out.println();
+
 
     }
 }
 /// testando usando o java 25 e java 20 testando as funcionalidades
 /// Aqui voce vai ver os tipos de variaveis INT DUBLE CHAR BOOLEAN STRING ISSO DAQUI E O BASICO DO BASICO
 /// AQUI PODE APLICAR O PADRAO CAMEL CASE TAMBEM CASO VOCE QUEIRA
+/// LOCALE E PARA DEFINIR O LOCAL DEFAULT US PARA TER PONTO NAS
