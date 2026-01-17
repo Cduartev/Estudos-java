eb31bc68787eb67bcdc428975d4b5add0c590f04
Cduartev <cayooduartevidal@gmail.com>
2026-01-17 11:06:36 -0300

    Formatacao para entende melhor cada codigo

--- untitled/src/projeto_variaveis_java_aula_01.java
+++ untitled/src/projeto_variaveis_java_aula_01.java
@@ -2,8 +2,8 @@ import java.util.Locale;
 
 public class projeto_variaveis_java_aula_01 {
     static void main() {
-        String produto1="Computador" ;
-        String produto2="Teclado";
+        String produto1 = "Computador";
+        String produto2 = "Teclado";
 
         int idade = 30;
         int codigo = 2020;
@@ -11,21 +11,23 @@ public class projeto_variaveis_java_aula_01 {
         double preco1 = 2100.50;
         double preco2 = 650.50;
         double medida = 54.3213;
-
+        IO.println("\n");
         System.out.println("Produtos: ");
-        System.out.println("o Preco do "+produto1 + " e igual a " + preco1);
-        System.out.println("O preco do "+produto2+" e igual a "+ preco2);
+        System.out.println("o Preco do " + produto1 + " e igual a " + preco1);
+        System.out.println("O preco do " + produto2 + " e igual a " + preco2);
 
-        IO.println("Registro: " + idade + " Anos, codigo "+ codigo + " e Genero: " + sexo);
+        IO.println("Registro: " + idade + " Anos, codigo " + codigo + " e Genero: " + sexo);
 
         IO.print("Codigo com 8 casas deciamais: ");
-        System.out.printf("%.8f%n",medida);
+        System.out.printf("%.8f%n", medida);
         IO.print("COM 3 CASAS DECIMAIS: ");
-        System.out.printf("%.3f%n",medida);
+        System.out.printf("%.3f%n", medida);
         IO.print("COM PONTOS DECIMAIS: ");
         Locale.setDefault(Locale.US);
-        IO.println(medida);
+        IO.print(medida);
     }
 }
 
 ///  LOCALE SERVE PARA ADICIOAR OS PONTOS DECIMAIS DE ACORDO COM A LOCALIDADE
+
+/// iniciar processamento de dados ////
