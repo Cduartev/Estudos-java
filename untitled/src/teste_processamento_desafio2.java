80e11d46ba8bd241833ef8786bef4c23e8e09073
Cduartev <cayooduartevidal@gmail.com>
2026-01-19 11:03:13 -0300

    PRIMEIRO CODIGO CALCULANDO INTENS

---
+++ untitled/src/teste_processamento_desafio2.java
@@ -0,0 +1,12 @@
+public class teste_processamento_desafio2 {
+    static void main() {
+        int x,y;
+        x = 5;
+        y = 2 * x;
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+
+
+}
