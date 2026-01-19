907fd00892e3fd8108c4abe051a0340add974609
Cduartev <cayooduartevidal@gmail.com>
2026-01-19 11:35:38 -0300

    Comentarios ajudando a fazer

--- untitled/src/entrada_de_dados.java
+++ untitled/src/entrada_de_dados.java
@@ -1,18 +1,21 @@
+import java.util.Locale;
 import java.util.Scanner;
 public class entrada_de_dados {
     static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+        String nome;
         int x;
+        nome = sc.nextLine();
         x = sc.nextInt();
-        System.out.println("Voce digitou o numero: " + x);
-
-
-        if (x <= 5) {
-            IO.println("Voce reprovou");
+        if (x >= 7) {
+            IO.println("Voce passou Parabens " + nome +" Sua nota: " + x);
             sc.close();
         } else
-            IO.println("Voce nao reprovou");
+            IO.println("Voce nao passou "+ nome + " Sua nota: " + x );
         sc.close();
     }
     }
-
+///  se o NEXT INT FOR PRIMEIRO QUE O NEXT LINE ELE CONSOME A QUEBRA DE LINHA SENAO ELE FICA COM UMA QUEBRA DE LINHA
+///  PENDENTE ENTAO CUIDADO NA HORA DE FOR USAR ISSO
